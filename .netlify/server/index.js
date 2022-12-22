dc568f0491d7cba7ff35c3432f13c0adb151a45c
Mears7 <jordan@vireocreative.ca>
2022-12-22 08:18:46 -0600

    mobile button fix

--- .netlify/server/index.js
+++ .netlify/server/index.js
@@ -2377,7 +2377,7 @@ class Server {
       app_template,
       app_template_contains_nonce: false,
       error_template,
-      version: "1671717546628"
+      version: "1671718713289"
     };
   }
   async init({ env }) {
